e60deac98f33566ac74c86923938a079f1b69dab
Florian Hinz <hinzflorian@protonmail.com>
2024-08-10 09:44:24 +0200

    now created trajectory with Kabsch at every step

--- prepocessing/ala2/merge_trajectory_data.py
+++ prepocessing/ala2/merge_trajectory_data.py
@@ -5,9 +5,15 @@ import os
 import torch
 import numpy as np
 from prepocessing.from_noe import rdmol_to_edge
+import mdtraj as md
 
 
 def calculate_increments(traj, stride):
+    increments = traj[stride:] - traj[:-stride]
+    return increments
+
+
+def calculate_increments_with_alignment(traj, stride):
     """Calculate the displacement
 
     Args:
@@ -16,8 +22,28 @@ def calculate_increments(traj, stride):
     Returns:
         increments the increments between x_{i+stride} and x_{i}
     """
-    increments = traj[stride:] - traj[:-stride]
-    return increments
+    # alignmen
+    increments = []
+    for ind in range(len(traj) - stride):
+        print(ind)
+        two_element_traj = torch.cat(
+            [traj[ind, None, :], traj[ind + stride, None, :]], dim=0
+        )
+        aligned_elements = align_traj(two_element_traj)
+        increment = aligned_elements[1] - aligned_elements[0]
+        increments.append(increment)
+    increments_tensor = torch.tensor(increments)
+    return increments_tensor
+
+
+def align_traj(traj):
+    md_traj = md.Trajectory(np.array(traj), topology=None)
+    # Use the first frame as the reference for alignment
+    reference_frame = md_traj[0]
+    # Superpose all frames to the reference frame
+    aligned_traj = md_traj.superpose(reference_frame).xyz
+    return aligned_traj
+
 
 
 def create_increments_and_merge(traj_dir, stride, output_dir):
@@ -35,7 +61,10 @@ def create_increments_and_merge(traj_dir, stride, output_dir):
     for ind in range(10):
         traj_path = os.path.join(traj_dir, f"trajectory_coords-{ind}.npy")
         traj = torch.tensor(np.load(traj_path))
-        increments = calculate_increments(traj, stride)
+        traj = align_traj(traj)
+        traj = torch.tensor(traj)
+        increments = calculate_increments_with_alignment(traj, stride)
+
         trajectory_merged.append(traj[:-stride])
         increments_merged.append(increments)
     trajectory_merged_tensor = torch.cat(trajectory_merged)
